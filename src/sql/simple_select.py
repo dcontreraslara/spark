cbf0bccb341211197b3364b44c34702f19367c5c
Dario Alonso Contreras Lara <dario.contreras@globant.com>
2020-01-10 11:25:09 -0300

    simple spaek sql select query added

---
+++ src/sql/simple_select.py
@@ -0,0 +1,20 @@
+from pyspark.sql import SparkSession, Window
+from pyspark.sql import functions as f
+
+spark = SparkSession.builder\
+     .master('local')\
+     .appName('test etl')\
+     .getOrCreate()
+
+data = [(1, 1, 'proda'),
+        (1, 2, 'prodb'),
+        (1, 3, 'prodc'),
+        (2, 1, 'proda'),
+        (2, 2, 'prodb'),
+        (2, 2, 'prodc')]
+
+df = spark.createDataFrame(data, ["id", "sorted_col", "product"])
+df.registerTempTable("simpleTable")
+
+result = spark.sql("select * from simpleTable where id = 1")
+result.show()
